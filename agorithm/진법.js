1477f998049efc825201753d21c3c88d3ef7c4f1
Kwan-Ung Park <pouu69@gmail.com>
2017-03-03 21:50:43 +0900

    Update 진법.js

--- agorithm/진법.js
+++ agorithm/진법.js
@@ -1,3 +1,4 @@
+//  진법
 function prop(n,b){
   var ret = '';
   
@@ -15,3 +16,22 @@ function prop(n,b){
 }
 
 console.log(prop(10,2));
+
+// 역진법
+function reProp(n,b){
+  var ans = "";
+  for(var i=0 ; i<n.length ; i++){
+    var c = n[i];
+    if(0 <= (+c) && (+c) <= 9){
+      ans = +ans * b + (+c);
+     // console.log(ans);
+    }else{
+      ans = +ans * b + ( (+c)+10 - (+('A'.charCodeAt(0))) );
+     //  console.log(ans);
+    }
+  }
+  
+  console.log(ans);
+}
+
+reProp("102",3);
